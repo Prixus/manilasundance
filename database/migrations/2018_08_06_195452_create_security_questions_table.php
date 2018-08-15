c20df0296fd0571e2120b3f280a8d20b0626e972
Simon Peter Calamno <simonpetercalamno@gmail.com>
2018-08-15 17:16:49 +0800

    The Security Questions table has been updated

--- database/migrations/2018_08_06_195452_create_security_questions_table.php
+++ database/migrations/2018_08_06_195452_create_security_questions_table.php
@@ -15,7 +15,7 @@ class CreateSecurityQuestionsTable extends Migration
     {
         Schema::create('security_questions', function (Blueprint $table) {
             $table->increments('PK_SecurityID');
-            $table->enum('Security_Question',['What is the name of your first dog?','What is the name of your first crush?','At what age did you have your first pet?','What is the full-name of your favorite teacher?']);
+            $table->string('Security_Question',255);
             $table->string('Security_Answer',255);
             $table->integer('FK_AccountID')->unsigned();
             $table->foreign('FK_AccountID')->references('PK_AccountID')->on('accounts')->onDelete('cascade')->onUpdate('cascade');
